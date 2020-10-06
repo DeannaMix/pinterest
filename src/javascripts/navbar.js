97d998632544864936a2c548116b1901ae13c106
Deanna Mix <deanna@LAPTOP-PG68556H.localdomain>
2020-10-06 14:55:20 -0500

    added new navbar that displays users and boards and add board button

--- src/javascripts/navbar.js
+++ src/javascripts/navbar.js
@@ -1,9 +1,33 @@
 const navbar = () => {
   $('#nav').html(
-    `<nav class="navbar fixed-top navbar-light" style="background-color: #8499B1;">
-        <span class="navbar-brand mb-0 h1" style="color: #DBCFB0;">Welcome to Pinterest</span>
-        <button type="button" class="nav-link btn btn-link" id="nav-logout-btn">Logout</button>
-      </nav>`
+    `<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light" style="background-color: #8499B1;">
+    <a class="navbar-brand" href="#">Barky Pinterest</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+    aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+    <li class="nav-item mx-3"  id="users-link">
+      <a class="nav-link" href="#"><i class="fas fa-dog"></i> Users</a>
+    </li>
+    <li class="nav-item mx-3" id="boards-link">
+      <a class="nav-link" href="#"><i class="far fa-"></i><i class="fas fa-bone"></i> Boards</a>
+    </li>
+    <li class="nav-item mx-3" id="add-board-link">
+      <a class="nav-link" href="#"><i class="fas fa-plus-circle"></i> Add A Board</a>
+    </li>
+  </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="user-info-nav">
+          Welcome,!
+        </li>
+        <li class="nav-item">
+          <button class="nav-link btn btn-link p-2" id="nav-logout-btn">Logout</button>
+        </li>
+      </ul>
+    </div>
+  </nav>`
   );
 };
 
